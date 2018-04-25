e3625ce8f9112cab01b2def6864d24b927912de7
DevOps <DevOps.Services@aa.com>
2018-04-25 12:49:16 +0530

    Update code

--- sample_program/src/sample_program/Sumofnumbers.java
+++ sample_program/src/sample_program/Sumofnumbers.java
@@ -1,11 +1,6 @@
-package sample_program;
-
-
 public class Sumofnumbers {
-
-	public int add(int a,int b){
-		return a+b;
-	}
-	
+ 
+ 	public static void main(String args[]){
+		System.out.println("Sum of 5 and 5 is 10");
+ 	}
 }
-
